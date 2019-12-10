c220b58c687c40ced1e92e54913680fd7c1cfd72
Alain CAO <alaincao17@gmail.com>
2019-12-10 03:07:29 +0100

    Simple example BTRFS snapshot/backups

--- Readme.md
+++ Readme.md
@@ -1,2 +1,16 @@
 
-Simple NodeJS-Electron application that can be used for quick testing environment or as starting point for an application
+Requirements:
+- `btrfs-prog` and a BTRFS filesystem (with subvolumes) ...
+- `node` & `npm`
+- [`pigz`](https://zlib.net/pigz/) (i.e. 'apt-get install pigz')
+
+Compile:
+```bash
+$ npm install
+$ ./node_modules/.bin/gulp
+```
+
+Run:
+```bash
+$ ./test.js
+```
--- test.ts
+++ test.ts
@@ -1,15 +1,17 @@
 
-import * as path from 'path';
-import * as fs from 'fs';
-import * as moment from 'moment';
 import Log from './logger';
 import * as common from './common';
 
-import * as azure from './azure';
 import * as backup from './backup';
-import * as bosun from './bosun';
 import * as btrfs from './btrfs';
-import * as sensors from './sensors';
+
+const subvolumes = [
+		{ name:'homes',	path:'/path/to/homes/' },
+		{ name:'data',	path:'/path/to/data/' },
+	];
+const snapshotsDir		= '/path/to/snapshots/';
+const backupsDir		= '/path/to/backups/';
+const backupsTestDir	= '/path/to/backups/tests/';
 
 async function main() : Promise<void>
 {
@@ -18,8 +20,67 @@ async function main() : Promise<void>
 	try
 	{
 		common.init({ log });
+		btrfs.config.useSudo = true;
 
-		log.log( 'Hello world' );
+		let isHourly = false;
+		let isDaily = false;
+		if( common.NOW.minute() == 0 )
+		{
+			// Hourly is XX:00
+			isHourly = true;
+
+			if( common.NOW.hour() == 7 )
+			{
+				// Daily is 07:00
+				isDaily = true;
+			}
+		}
+
+		// Example hourly snapshots:
+
+		const snapshotTasks : Promise<void>[] = [];
+		if( isHourly )
+		{
+			log.log( 'Launch snapshots tasks' );
+			subvolumes.forEach( item=>
+				{
+					const task = backup.runSnapshotRequest( log, {
+										name				: item.name,
+										subvolume			: item.path,
+										snapshotsDir		: snapshotsDir,
+										snapshotsRotation	: backup.snapshotsRotation_timeMachine,
+									} );
+					snapshotTasks.push( task );
+				} );
+		}
+		log.log( 'Wait for snapshots tasks to terminate' );
+		await Promise.all( snapshotTasks );
+		log.log( 'Snapshots tasks terminated' );
+
+		// Example daily backups
+
+		const backupTasks : Promise<void>[] = [];
+		if( isDaily )
+		{
+			log.log( 'Launch backup tasks' );
+			subvolumes.forEach( item=>
+				{
+					const task = backup.runBackupRequest( log, {
+							name					: item.name,
+							sourceSnapshotsDir		: snapshotsDir,
+							sourceSnapshotsRemove	: false,  // nb: Snapshots are already rotated ; Set to true if all backuped snapshots (except the last one) are to be deleted
+							destinationBackupsDir	: backupsDir,
+							destinationSnapshot		: {  // Optional/recommended: Extract (i.e. "btrfs receive") the backups so the backups are also TESTED
+														dir			: backupsTestDir,
+														rotation	: backup.snapshotsRotation_keepOnlyLast1,
+													},
+						} );
+					backupTasks.push( task );
+				} );
+		}
+		log.log( 'Wait for backups tasks to terminate' );
+		await Promise.all( backupTasks );
+		log.log( 'Backups tasks terminated' );
 
 		log.log( 'Exit' );
 	}
