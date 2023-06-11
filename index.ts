2255cd708c65229ec84d3d3b3aea4693ce327183
Alain CAO <alaincao17@gmail.com>
2023-06-11 19:32:29 +0000

    Typescript initial

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get( '/', (req,res)=>
+	{
+		res.send( 'Marchage!' );
+	} );
+
+app.listen( 5000, ()=>
+	{
+		console.log( `⚡️Server running @ port 5000`);
+	} );
