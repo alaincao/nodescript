b2347f7620c53544acd6f0ff10b0f806437cb3fd
Alain CAO <alaincao17@gmail.com>
2024-08-16 00:18:22 +0200

    Improvements & base for a web application

---
+++ stop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export APP_DIR=$(dirname $(readlink -f "$0"))
+
+set -xe
+
+cd "${APP_DIR}/"
+
+PARM="stop"
+if [ "$1" == "down" ]; then
+	PARM="down"
+fi
+
+docker compose "${PARM}"
